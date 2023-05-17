d5d86deb7d6f6db7d231a8e94838f86d4d0eea70
dmitriy-kozik <dmitriy-kozik@ukr.net>
2023-05-17 22:44:35 +0300

    Single Responsibility

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-phpinfo();
+
+class Product {}
+
+class ProductGet extends Product {
+    public function get(name) {}
+}
+class ProductAction extends Product {
+    public function set(name, value) {}
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+}
+class ProductView extends Product {
+    public function show() {}
+    public function print() {}
+}
