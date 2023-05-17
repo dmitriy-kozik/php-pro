dd9e7e722b94fc42f308b130c045597ee44589df
dmitriy-kozik <dmitriy-kozik@ukr.net>
2023-05-17 23:32:13 +0300

    Open/Closed Principle

--- index.php
+++ index.php
@@ -1,15 +1,72 @@
 <?php
 
-class Product {
-    public function get(name) {}
-    public function set(name, value) {}
+
+interface Format
+{
+    public function format($string);
 }
-class ProductAction extends Product {
-    public function save() {}
-    public function update() {}
-    public function delete() {}
+class FormatRaw implements Format
+{
+    public function format($string)
+    {
+        return $string;
+    }
 }
-class ProductView extends Product {
-    public function show() {}
-    public function print() {}
+class FormatWithDate implements Format
+{
+    public function format($string)
+    {
+        return date('Y-m-d H:i:s') . $string;
+    }
 }
+class FormatWithDateAndDetails implements Format
+{
+    public function format($string)
+    {
+        return date('Y-m-d H:i:s') . $string . ' - With some details';
+    }
+}
+
+
+interface Deliver
+{
+    public function deliver($format);
+}
+
+class DeliverByEmail implements Deliver
+{
+    public function deliver($format)
+    {
+        echo "Вывод формата ({$format}) по имейл";
+    }
+}
+class DeliverBySMS implements Deliver
+{
+    public function deliver($format)
+    {
+        echo "Вывод формата ({$format}) в смс";
+    }
+}
+class DeliverToConsole implements Deliver
+{
+    public function deliver($format)
+    {
+        echo "Вывод формата ({$format}) в консоль";
+    }
+}
+
+
+class Logger
+{
+    public function __construct(protected Format $format, protected Deliver $deliver)
+    {
+    }
+
+    public function log($string)
+    {
+        $this->deliver->deliver($this->format->format($string));
+    }
+}
+$logger = new Logger(new FormatRaw(), new DeliverBySMS());
+$logger->log('Emergency error! Please fix me!');
+
